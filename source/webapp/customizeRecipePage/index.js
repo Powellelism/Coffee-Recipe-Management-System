672c56413fa844220be76eff5785efee0eea5692
ekirimlioglu <80229265+ekirimlioglu@users.noreply.github.com>
2024-05-02 13:20:35 -0700

    common js is required but script.js is not

--- source/webapp/customizeRecipePage/index.js
+++ source/webapp/customizeRecipePage/index.js
@@ -1,8 +1,6 @@
 import './script.js'
 import './style.css'
-// Import common JavaScript files
-const commonJsContext = require.context('../common/js', true, /\.js$/);
-commonJsContext.keys().forEach(commonJsContext);
+import '../common/js/common.js'
 
 // Import common CSS files
 const commonCssContext = require.context('../common/css', true, /\.css$/);
