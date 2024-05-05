1ff65623cfe96ed986c51a86031d2eb4085689df
ekirimlioglu <80229265+ekirimlioglu@users.noreply.github.com>
2024-05-05 15:03:55 -0700

    ignores syntax

--- source/webapp/eslint.config.mjs
+++ source/webapp/eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default {
-  ignorePatterns: ["test/"], // Ignore all files within the test directory
+  ignores: ["test/*"], // Ignore all files within the test directory
   languageOptions: {
     globals: globals.browser
   },
