2867faf42e035131656ba03eee7679c5c416d8aa
rahuljones <rahuljjones@gmail.com>
2024-05-09 21:47:53 -0700

    Added middleware

--- source/middlewares/authMiddleware.js
+++ source/middlewares/authMiddleware.js
@@ -1,16 +1,18 @@
-//const supabase = require('../config/supabaseClient');
+const supabase = require('../config/supabaseClient');
 
-/**
- * This function authenticates the user using Supabase authentication.
- * @param request
- * @param response
- * @param next
- * @returns {Promise<void>}
- */
 exports.authenticate = async (request, response, next) => {
-  // right now, we are not using this middleware for anything all the routes are public
-  // we should check if token is valid and if user is logged in to serve the next route
-  next();
-  // suppress the eslint warning that supabase is not used will be used to authenticate user by commenting line 1
-  // assignee should uncomment line 1 and implement the authentication logic
+  const token = request.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return response.status(401).send({ message: 'Did not get a token' });
+  }
+  try {
+    const {data: session, error} = await supabase.auth.api.getUser(token);
+    if (error) {
+      throw new Error('Token does not work');
+    }
+    request.user = session.user;
+    next();
+  } catch (error) {
+    return response.status(401).send({ message: 'Authentication failed: ' + error.message });
+  }
 };
--- source/routes/auth.js
+++ source/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
