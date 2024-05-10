cd8e6949200b6e2a272dc2395b30c0d4fc8fb80a
adarsh249 <adarsh249@users.noreply.github.com>
2024-05-10 01:57:23 +0000

    Apply automatic changes

--- source/routes/views.js
+++ source/routes/views.js
@@ -26,9 +26,7 @@ router.get("/register", (req, res) => {
 
 // Serve forgot password view
 router.get("/forgot-password", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../public/templates", "auth/forgot.html"),
-  );
+  res.sendFile(path.join(__dirname, "../public/templates", "auth/forgot.html"));
 });
 
 // Serve common home view
