5c28a62de933372da436317df212c3db4cbbcd6f
rahuljones <rahuljjones@gmail.com>
2024-05-16 18:47:06 -0700

    added middleware to login and register

--- source/routes/auth.js
+++ source/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post("/login", authController.login);
-router.post("/register", authController.register);
+router.post("/login", authMiddleware, authController.login);
+router.post("/register", authMiddleware, authController.register);
 
 module.exports = router;
