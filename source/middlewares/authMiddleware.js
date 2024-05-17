8cb5bbed51b31ae5809bfb59680fb3935088f07d
rahuljones <rahuljones@users.noreply.github.com>
2024-05-17 01:47:51 +0000

    Apply automatic changes

--- source/middlewares/authMiddleware.js
+++ source/middlewares/authMiddleware.js
@@ -1,18 +1,20 @@
-const supabase = require('../config/supabaseClient');
+const supabase = require("../config/supabaseClient");
 
 exports.authenticate = async (request, response, next) => {
-  const token = request.headers.authorization?.split(' ')[1];
+  const token = request.headers.authorization?.split(" ")[1];
   if (!token) {
-    return response.status(401).send({ message: 'Did not get a token' });
+    return response.status(401).send({ message: "Did not get a token" });
   }
   try {
-    const {data: session, error} = await supabase.auth.api.getUser(token);
+    const { data: session, error } = await supabase.auth.api.getUser(token);
     if (error) {
-      throw new Error('Token does not work');
+      throw new Error("Token does not work");
     }
     request.user = session.user;
     next();
   } catch (error) {
-    return response.status(401).send({ message: 'Authentication failed: ' + error.message });
+    return response
+      .status(401)
+      .send({ message: "Authentication failed: " + error.message });
   }
 };
