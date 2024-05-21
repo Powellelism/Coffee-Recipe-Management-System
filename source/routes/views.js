6f5b6309a72fbff95f9f547ba5b7ab8c970a6fae
adarsh249 <adarshpatel249@gmail.com>
2024-05-20 19:21:44 -0700

    removed terrible file structure

--- source/routes/views.js
+++ source/routes/views.js
@@ -4,70 +4,70 @@ const router = express.Router();
 
 // Serve index view
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "index.html"));
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 // Serve about us view
 router.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "about.html"));
+  res.sendFile(path.join(__dirname, "../public/pages", "about.html"));
 });
 
 // Serve login view
 router.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "auth/login.html"));
+  res.sendFile(path.join(__dirname, "../public/pages", "auth/login.html"));
 });
 
 // Serve register view
 router.get("/register", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../public/templates", "auth/register.html"),
+    path.join(__dirname, "../public/pages", "auth/register.html"),
   );
 });
 
 // Serve forgot password view
 router.get("/forgot", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "auth/forgot.html"));
+  res.sendFile(path.join(__dirname, "../public/pages", "auth/forgot.html"));
 });
 
 // Serve common home view
 router.get("/common/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "common.html"));
+  res.sendFile(path.join(__dirname, "../public/pages", "common.html"));
 });
 
 // Serve dashboard view
 router.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "dashboard.html"));
+  res.sendFile(path.join(__dirname, "../public/pages", "dashboard.html"));
 });
 // serve preset customize view
 router.get("/preset/customize", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../public/templates", "presetCustomize.html"),
+    path.join(__dirname, "../public/pages", "presetCustomize.html"),
   );
 });
 
 // serve preset list view
 router.get("/preset/list", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/templates", "presetList.html"));
+  res.sendFile(path.join(__dirname, "../public/pages", "presetList.html"));
 });
 
 // serve recipe customization view
 router.get("/recipe/customize", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../public/templates", "customizeRecipe2.html"),
+    path.join(__dirname, "../public/pages", "customizeRecipe.html"),
   );
 });
 
 // serve saved recipe view
 router.get("/recipe/saved", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../public/templates", "savedRecipes.html"),
+    path.join(__dirname, "../public/pages", "savedRecipes.html"),
   );
 });
 
 // serve recipe review view
 router.get("/recipe/review", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../public/templates", "reviewRecipe2.html"),
+    path.join(__dirname, "../public/pages", "reviewRecipe.html"),
   );
 });
 module.exports = router;
