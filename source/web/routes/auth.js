c010794a49bde2b9c0a0713c22779dccdf88c080
ekirimlioglu <80229265+ekirimlioglu@users.noreply.github.com>
2024-05-30 19:12:10 -0700

    update the directory structure

--- source/web/routes/auth.js
+++ source/web/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/login", authMiddleware.authenticate, authController.login);
 router.post("/register", authMiddleware.authenticate, authController.register);
