f9a4f5821831268ac88028209df19ad4d88202ca
adarsh249 <adarshpatel249@gmail.com>
2024-06-04 13:51:13 -0700

    adding more testing

--- source/web/__tests__/current/createRecipe.test.js
+++ source/web/__tests__/current/createRecipe.test.js
@@ -8,4 +8,14 @@ describe("Test customize recipe page functionality", () => {
         const focusedElement = await page.evaluate(() => document.activeElement.id);
         expect(focusedElement).toBe('recipe-name');
     });
+
+    it('Testing we can add ingredients to the recipe', async () => {
+        const addIngredientButton = await page.$('#add-ingredient');
+        await addIngredientButton.click();
+        const ingredientInput = await page.$('.ingredients-list input:nth-of-type(2)');
+        await ingredientInput.click();
+        await ingredientInput.type('Test Ingredient');
+        const ingredientInputs = await page.$$('.ingredients-list input');
+        expect(ingredientInputs.length).toBe(2);
+    });
 });
