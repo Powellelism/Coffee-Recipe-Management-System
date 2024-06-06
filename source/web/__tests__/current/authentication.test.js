2338db3cb95f662f2d256811a5d115f445ec9d8d
adarsh249 <adarshpatel249@gmail.com>
2024-06-06 10:32:56 -0700

    pushing changes to .yml file

--- source/web/__tests__/current/authentication.test.js
+++ source/web/__tests__/current/authentication.test.js
@@ -31,25 +31,27 @@ describe("Test frontend and backend for logging in and registering", () => {
     expect(errorMessageText).toBe("Incorrect email or password.");
   });
 
-  it("Test forgot password and redirect toward login", async () => {
-    await page.goto("http://localhost:3000/login");
-    await page.waitForSelector("a.forgot");
-    const forgotPassword = await page.$("a.forgot");
-    await forgotPassword.click();
-    await page.waitForNavigation();
-    const forgotURL = page.url();
-    expect(forgotURL).toBe("http://localhost:3000/forgot");
-    await page.waitForSelector("body > p:first-of-type");
-    const forgotPasswordText = await page.$eval(
-      "body > p:first-of-type",
-      (el) => el.innerText,
-    );
-    expect(forgotPasswordText).toBe("TOO BAD, FIGURE IT OUT 😈");
-    await page.waitForSelector('a[href="/login"]');
-    const loginButton = await page.$('a[href="/login"]');
-    await loginButton.click();
-    await page.waitForNavigation();
-    const loginURL = page.url();
-    expect(loginURL).toBe("http://localhost:3000/login");
-  }, 40000);
+    it("Test forgot password and redirect toward login", async () => {
+      await page.goto("http://localhost:3000/login");
+      await page.waitForSelector("a.forgot");
+      const forgotPassword = await page.$("a.forgot");
+      await forgotPassword.click();
+      await page.waitForNavigation();
+      const forgotURL = page.url();
+      expect(forgotURL).toBe("http://localhost:3000/forgot");
+      await page.waitForSelector("body > p:first-of-type");
+      const forgotPasswordText = await page.$eval(
+        "body > p:first-of-type",
+        (el) => el.innerText,
+      );
+      expect(forgotPasswordText).toBe("TOO BAD, FIGURE IT OUT 😈");
+      await page.waitForSelector('a[href="/login"]');
+      const loginButton = await page.$('a[href="/login"]');
+      await loginButton.click();
+      await page.waitForNavigation();
+      const loginURL = page.url();
+      expect(loginURL).toBe("http://localhost:3000/login");
+    }, 40000);
+
+    
 });
