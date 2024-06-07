396ab87d9e3f75215d4d3c1286e4755e31641cf8
chau pham <c6pham@ucsd.edu>
2024-06-06 23:50:53 -0700

    update all the needed backend for recipe and rating. add mocked data

--- source/web/controllers/recipeController.js
+++ source/web/controllers/recipeController.js
@@ -2,12 +2,13 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 /**
- * This function retrieves the recipes with their associated data.
- * @param request
- * @param response
+ * This function retrieves the recipes based on descending rating.
+ * @param request None
+ * @param response return a list of recipes in descending rating. 
+ * Recipe is in json file including name, description, food, ingredients, size, rating, reviews, instructions, totalTime)
  * @returns {Promise<void>}
  */
-exports.getRecipes = async (request, response) => {
+exports.getRatingRecipes = async (request, response) => {
   try {
     const recipes = await prisma.recipe.findMany({
       include: {
@@ -30,11 +31,6 @@ exports.getRecipes = async (request, response) => {
             name: true,
           },
         },
-        ratings: {
-          select: {
-            score: true,
-          },
-        },
         reviews: {
           select: {
             content: true,
@@ -42,7 +38,7 @@ exports.getRecipes = async (request, response) => {
         },
       },
       orderBy: {
-        name: "asc",
+        rating: "desc",
       },
     });
 
@@ -59,7 +55,74 @@ exports.getRecipes = async (request, response) => {
       },
       ingredients: recipe.ingredients.map((ingredient) => ingredient.name),
       size: recipe.size,
-      ratings: recipe.ratings.map((rating) => rating.score),
+      rating: recipe.rating,
+      reviews: recipe.reviews.map((review) => review.content),
+      instructions: recipe.instructions,
+      totalTime: recipe.totalTime,
+    }));
+
+    response.json(formattedRecipes);
+  } catch (error) {
+    console.error("Error retrieving recipes:", error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+/**
+ * This function retrieves the recipes based on created time, most recent recipe will show up first.
+ * @param request None
+ * @param response return a list of recipes in descending id number. 
+ * Recipe is in json file including name, description, food, ingredients, size, rating, reviews, instructions, totalTime)
+ * @returns {Promise<void>}
+ */
+exports.getRecentRecipes = async (request, response) => {
+  try {
+    const recipes = await prisma.recipe.findMany({
+      include: {
+        food: {
+          select: {
+            name: true,
+            description: true,
+            price: true,
+            store: {
+              select: {
+                name: true,
+              },
+            },
+            type: true,
+            size: true,
+          },
+        },
+        ingredients: {
+          select: {
+            name: true,
+          },
+        },
+        reviews: {
+          select: {
+            content: true,
+          },
+        },
+      },
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    const formattedRecipes = recipes.map((recipe) => ({
+      recipeName: recipe.name,
+      description: recipe.description,
+      food: {
+        name: recipe.food.name,
+        description: recipe.food.description,
+        price: recipe.food.price,
+        store: recipe.food.store.name,
+        type: recipe.food.type,
+        size: recipe.food.size,
+      },
+      ingredients: recipe.ingredients.map((ingredient) => ingredient.name),
+      size: recipe.size,
+      rating: recipe.rating,
       reviews: recipe.reviews.map((review) => review.content),
       instructions: recipe.instructions,
       totalTime: recipe.totalTime,
@@ -74,8 +137,9 @@ exports.getRecipes = async (request, response) => {
 
 /**
  * This function adds a new recipe.
- * @param request
- * @param response
+ * @param request JSON body of a recipe including 
+ * name, description, foodId, ingredients, size, intructions, totalTime, userID
+ * @param response JSON file of the new created recipe 
  * @returns {Promise<void>}
  */
  exports.addRecipe = async (request, response) => {
@@ -128,30 +192,43 @@ exports.getRecipes = async (request, response) => {
   }
 };
 
+/**
+ * This function updates a recipe information.
+ * @param request JSON body key and value that needed to be updated. 
+ * Example: {"name": "new Name", "size": "Small"} if you want to update 
+ * the name of this recipe to "new Name" and the size to "Small"
+ * @param response JSON file of the recipe with new updated information
+ * @returns {Promise<void>}
+ */
 exports.updateRecipe = async (request, response) => {
   try {
     const newInfo = request.body;
+    const id = parseInt(request.params.id);
 
     // Update the recipe
     const recipe = await prisma.recipe.update({
       where: {
-        id: newInfo.id,
+        id: id,
       },
-      data: {
-        newInfo
-      },
+      data: newInfo,
     });
 
     response.status(201).json(recipe);
   } catch (error) {
-    console.error("Error adding recipe:", error);
+    console.error("Error update recipe:", error);
     response.status(500).json({ error: "Internal Server Error" });
   }
 };
 
+/**
+ * This function get recipe by id, id should be included in route
+ * @param request None
+ * @param response JSON of the recipe based on id
+ * @returns {Promise<void>}
+ */
 exports.getSingleRecipe = async (request, response) => {
   try {
-    const {id} = request.body;
+    const id = parseInt(request.params.id);
 
     // Get one recipe
     const recipe = await prisma.recipe.findUnique({
@@ -162,7 +239,61 @@ exports.getSingleRecipe = async (request, response) => {
 
     response.status(201).json(recipe);
   } catch (error) {
-    console.error("Error adding recipe:", error);
+    console.error("Error getting recipe:", error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+/**
+ * This function get rating by recipe based on recipe id
+ * @param request None
+ * @param response int of the current rating of the recipe
+ * @returns {Promise<void>}
+ */
+exports.getRatingForRecipe = async (request, response) => {
+  try {
+    const id = parseInt(request.params.id);
+
+    // Get recipe
+    const recipe = await prisma.recipe.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    
+    // Return the rating
+    response.status(201).json(recipe.rating);
+  } catch (error) {
+    console.error("Error getting ratings:", error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+/**
+ * This function update recipe rating
+ * @param request newRating with the new number of rating that need to be updated.
+ * Example: {"newRating": 3} if we want to update the rating of this recipe to 3
+ * @param response the value of new rating
+ * @returns {Promise<void>}
+ */
+exports.updateRecipeRating = async (request, response) => {
+  try {
+    const {newRating} = request.body;
+    const id = parseInt(request.params.id);
+
+    // Update the recipe rating
+    const recipe = await prisma.recipe.update({
+      where: {
+        id: id,
+      },
+      data: {
+        rating: newRating,
+      },
+    });
+    
+    response.status(201).json(recipe.rating);
+  } catch (error) {
+    console.error("Error updating rating:", error);
     response.status(500).json({ error: "Internal Server Error" });
   }
 };
